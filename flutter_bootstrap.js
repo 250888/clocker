71ffb6c9602503dac63e6f5b3d79a2b0c079f531
clocker <3807440790@qq.com>
2026-04-30 14:59:04 +0800

    Deploy: loading screen + Web Audio API + camera fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,12 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1479409164" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3335947136"
+  },
+  onEntrypointLoaded: function(engineInitializer) {
+    engineInitializer.initializeEngine().then(function(appRunner) {
+      appRunner.runApp();
+      if (window._flutterLoadingDone) window._flutterLoadingDone();
+    });
   }
 });
